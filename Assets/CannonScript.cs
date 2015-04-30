1df53c74e07ff895efe62c8a989ef6bf52a221ed
danben4 <danielbenediktsson4@gmail.com>
2015-04-30 21:25:20 +0000

    Addeda a canon to the ship and created a CannonScript

---
+++ Assets/CannonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonScript : MonoBehaviour {
+
+	public float fireRate = 0;
+	public float damage = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
