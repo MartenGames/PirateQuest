f7e70fbd33194d3f7e49edb5711f3eda65258b63
sindrikaldal <sindrimars@gmail.com>
2015-05-01 21:38:34 +0000

    AI

--- Assets/Scripts/EnemyAIScript.cs
+++ Assets/Scripts/EnemyAIScript.cs
@@ -7,19 +7,12 @@ public class EnemyAIScript : MonoBehaviour {
 	public float Distance;
 
 	private Vector3 Player;
-	private Vector3 Enemy;
 
 	private Vector2 PlayerDirection;
-	private Vector2 EnemyDirection;
 
 	private float XDif;
 	private float YDif;
-	private float EnemyXDif;
-	private float EnemyYDif;
-
-
-
-
+	
 	void Start () {
 
 	}
@@ -27,16 +20,8 @@ public class EnemyAIScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-
 		//Assign the Player Vector to the position of the player
 		Player = GameObject.Find("Player").transform.position;
-
-
-		if ((Enemy = GameObject.Find ("Enemy").transform.position) == null) {
-			continue;
-		}
-
 
 		//A formula to rotate the enemy ships towards the player
 		Vector3 dir = Player - transform.position;
@@ -47,25 +32,12 @@ public class EnemyAIScript : MonoBehaviour {
 		XDif = Player.x - transform.position.x;
 		YDif = Player.y - transform.position.y;
 
-		//Find the distance between current enemy and another enemy
-		EnemyXDif = Enemy.x - transform.position.x;
-		EnemyYDif = Enemy.y - transform.position.y;
-
 		//Create new vectors with the recently calculated coordinates
 		PlayerDirection = new Vector2 (XDif, YDif);
-		EnemyDirection = new Vector2 (EnemyXDif, EnemyYDif);
-
-
-		//If a enemy is nearby then move from him
-		if (Vector2.Distance (Enemy, transform.position) < Distance) {
-			transform.position = (transform.position - Enemy).normalized * MoveSpeed + Enemy;
-
-		}
 
 		//Only move the enemy if he is at a certain distance from the player
-		if(CanMove()) {
 			transform.position += (Vector3)PlayerDirection.normalized * MoveSpeed * Time.deltaTime;
-		}
+
 	}
 
 	//Boolean that says if the enemy shop can move
