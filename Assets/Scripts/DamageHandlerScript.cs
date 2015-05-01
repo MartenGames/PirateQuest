a8d2643af134dccd619b336c71fc75e59583798f
danben4 <danielbenediktsson4@gmail.com>
2015-05-01 17:05:08 +0000

    Collistion script

--- Assets/Scripts/DamageHandlerScript.cs
+++ Assets/Scripts/DamageHandlerScript.cs
@@ -5,15 +5,36 @@ public class DamageHandlerScript : MonoBehaviour {
 
 	public int health = 1;
 
+	public float invulnerabilityTimer = 0;
+
+	int layer;
+
 	/*
 	void OnCollisionEnter2D() {
 		Debug.Log ("Collision!");
 	}
 	*/
 
+	void Start() {
+		layer = gameObject.layer;
+	}
+
 	void OnTriggerEnter2D() {
 		Debug.Log ("Trigger!");
+
+
 		health--;
+		invulnerabilityTimer = 2f;
+		gameObject.layer = 11;
+
+	}
+
+	void Update() {
+		invulnerabilityTimer -= Time.deltaTime;
+
+		if (invulnerabilityTimer <= 0) {
+			gameObject.layer = layer;
+		}
 
 		if (health <= 0) {
 			Die ();
