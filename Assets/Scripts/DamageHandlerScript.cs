6eff574ad2fe4465dbefe78b37ce000e888b532f
danben4 <danielbenediktsson4@gmail.com>
2015-05-01 16:46:51 +0000

    Different layers and stuff

---
+++ Assets/Scripts/DamageHandlerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageHandlerScript : MonoBehaviour {
+
+	public int health = 1;
+
+	/*
+	void OnCollisionEnter2D() {
+		Debug.Log ("Collision!");
+	}
+	*/
+
+	void OnTriggerEnter2D() {
+		Debug.Log ("Trigger!");
+		health--;
+
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+}
