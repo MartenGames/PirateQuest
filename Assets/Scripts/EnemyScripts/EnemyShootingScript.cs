2d7c42e2d50175cb94cba2b48fd954e64c4d81d4
danben4 <danielbenediktsson4@gmail.com>
2015-05-02 17:20:01 +0000

    make enmies shoot slower

--- Assets/Scripts/EnemyScripts/EnemyShootingScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyShootingScript.cs
@@ -5,17 +5,12 @@ using System.Collections;
 public class EnemyShootingScript : MonoBehaviour {
 
 	public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
-
 	public GameObject bulletPrefab;
-
 	public AudioClip gunShot;
-
+	public float coolDownTimer = 0;
+	public float fireDelay = 1f;
 	Transform Player;
 
-	public float fireDelay = 0.5f;
-
-	float coolDownTimer = 0;
-	
 	// Update is called once per frame
 	void Update () {
 
@@ -38,7 +33,6 @@ public class EnemyShootingScript : MonoBehaviour {
 		//Only shoot within a certain distance
 		if (distance < 10) {
 			if (coolDownTimer <= 0) {
-
 				coolDownTimer = fireDelay;
 
 				Vector3 offset = transform.rotation * bulletOffset;
