0f12eedf22a13fd77b7a3dfef2db9a61732c3fd0
danben4 <danielbenediktsson4@gmail.com>
2015-05-02 16:21:55 +0000

    second canon shooting

--- Assets/Scripts/SecondBulletScript.cs
+++ Assets/Scripts/SecondBulletScript.cs
@@ -1,23 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletScript : MonoBehaviour {
-
+public class SecondBulletScript : MonoBehaviour {
+	
 	float speed = 5;
 	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	void OnTriggerEnter2D() {
 		Destroy (gameObject);
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = transform.position;
-		Vector3 velocity = new Vector3 (speed * Time.deltaTime, 0, 0);
+		Vector3 velocity = new Vector3 (-(speed * Time.deltaTime), 0, 0);
 		pos += transform.rotation * velocity;
 		transform.position = pos;
 	}
