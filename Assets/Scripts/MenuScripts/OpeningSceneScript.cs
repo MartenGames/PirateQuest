ed7aac7b1275f65ffe786b5d54afea2788939b79
sindrikaldal <sindrimars@gmail.com>
2015-05-03 16:50:14 +0000

    opening scene

---
+++ Assets/Scripts/MenuScripts/OpeningSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine ("wait");
+
+	}
+	
+	// Update is called once per frame
+	IEnumerator wait () {
+		
+		yield return new WaitForSeconds(3);
+			Application.LoadLevel ("MainMenu");
+
+	}
+}
