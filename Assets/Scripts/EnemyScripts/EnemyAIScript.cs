dd8c4313f9738aa0b98e94ed37ec2bfb479f6d02
danben4 <danielbenediktsson4@gmail.com>
2015-05-05 18:26:05 +0000

    flot

--- Assets/Scripts/EnemyScripts/EnemyAIScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyAIScript.cs
@@ -17,7 +17,7 @@ public class EnemyAIScript : MonoBehaviour {
 		if (Player == null) {
 			GameObject go = GameObject.Find ("Player(Clone)");
 
-			if(go != null) {
+			if (go != null) {
 				Player = go.transform;
 			}
 		}	
@@ -38,6 +38,37 @@ public class EnemyAIScript : MonoBehaviour {
 		float distance = Vector3.Distance (Player.position, transform.position);
 		bool tmp = true;
 
+		/*
+		var direction = new Vector3(0, 45, 0);
+		var length = 10;
+		var diagonal = transform.TransformDirection(direction);
+		diagonal.Normalize();
+		Debug.DrawRay(transform.position, diagonal * length, Color.green);
+		//Debug.Log (Physics.Raycast (transform.position, diagonal, length));
+		if (Physics2D.Raycast(transform.position, diagonal, length)) {
+			Debug.Log ("There is something in front of the object!");
+		}
+		*/
+
+		//var hit: RaycastHit2D = Physics2D.Raycast(Vector2(transform.position.x,transform.position.y-2), -Vector2.up, raydistance );
+
+		
+		var length = 50;
+		var direction = new Vector2(0, 45);
+		//RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 10);
+		RaycastHit2D[] hit = Physics2D.RaycastAll(new Vector2(transform.position.x + 2, transform.position.y - 2), direction, length);
+		var diagonal = transform.TransformDirection(direction);
+		diagonal.Normalize ();
+		Debug.DrawRay(transform.position, diagonal * length, Color.green);
+
+		foreach (RaycastHit2D obj in hit) {
+			if(obj.collider.tag == "Island") {
+				Debug.Log ("HIT!");
+				Debug.Log (obj.collider.name);
+			}
+		}
+
+		
 		GameObject[] islands = GameObject.FindGameObjectsWithTag ("Island");
 
 		foreach (GameObject island in islands) {
@@ -46,15 +77,17 @@ public class EnemyAIScript : MonoBehaviour {
 			Vector3 desiredVector;
 			float radius = island.GetComponent<CircleCollider2D>().radius;
 			float e = 8;
-			// maybe this is not in the center of the island!!
+			// Maybe this is not in the center of the island!!
+			// Have to check it out!!
 			float d = Vector3.Distance(island.transform.position, transform.position);
 			float x = 0;
-
+			/*
 			Debug.Log ("Island name: "+ island.name);
 			Debug.Log ("Island position: " + island.transform.position.x + " " + island.transform.position.y);
 			Debug.Log ("Radius: " + radius);
 			Debug.Log ("d: " + d);
 			Debug.Log ("e: " + e);
+			*/
 
 			if(d < e) {
 				x = ((d - radius) / (e - radius));
