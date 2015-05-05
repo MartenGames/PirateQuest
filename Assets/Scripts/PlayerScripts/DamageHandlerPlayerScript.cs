0118c3a1ada84242dc223fbfec5520dd8fcb2dcb
hilmarragnars <hilmarragnars@gmail.com>
2015-05-05 14:01:52 +0000

    added some functions to the upgrading system

--- Assets/Scripts/PlayerScripts/DamageHandlerPlayerScript.cs
+++ Assets/Scripts/PlayerScripts/DamageHandlerPlayerScript.cs
@@ -43,7 +43,7 @@ public class DamageHandlerPlayerScript : MonoBehaviour {
 		goToMap = goToMap.GetComponent<Button> ();
 
 		GameObject cannon = GameObject.Find ("Cannon2");
-		cannon.SetActive (false);
+		cannon.SetActive (GameObject.Find("EmptyObject(Clone)").GetComponent<StoringVarScript>().secondCannon);
 		//This comment is for hilmar
 		//GameObject.Find("EmptyObject(Clone)").GetComponent<StoringVarScript>().secondCannon
 		layer = gameObject.layer;
