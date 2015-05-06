c786aae122d1eee0e829aa42f35c986e7efc8d70
hilmarragnars <hilmarragnars@gmail.com>
2015-05-06 17:57:39 +0000

    Health slider fixed

---
+++ Assets/Scripts/HealthSlide/HealthSlideInitializeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthSlideInitializeScript : MonoBehaviour {
+
+	public Slider healthSlider;
+
+
+	void Start () {
+		Debug.Log ("Does this happen\n");
+		Debug.Log (healthSlider.maxValue);
+		healthSlider.maxValue = GameObject.Find ("EmptyObject(Clone)").GetComponent<StoringVarScript> ().health + 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
