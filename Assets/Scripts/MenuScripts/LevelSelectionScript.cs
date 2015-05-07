3add9537ba4df5332de3b2ab56b1e8a145ccaaab
sindrikaldal <sindrimars@gmail.com>
2015-05-07 18:14:22 +0000

    better buttons in level selection

--- Assets/Scripts/MenuScripts/LevelSelectionScript.cs
+++ Assets/Scripts/MenuScripts/LevelSelectionScript.cs
@@ -4,24 +4,23 @@ using System.Collections;
 
 public class LevelSelectionScript : MonoBehaviour {
 
-	//public Button[] levels;
+	public Button[] levels;
 
 	void Start () {
 
-		/*GameObject emptyObject = GameObject.Find ("EmptyObject(Clone)");
+		GameObject emptyObject = GameObject.Find ("EmptyObject(Clone)");
 		int currentLevel = emptyObject.GetComponent<StoringVarScript> ().currentLevel;
 
 		//Grey out the levels that are not available to the player. 
 		for (int i = 0; i < levels.Length; i++) {
 			if((i + 1) == currentLevel) {
-				levels[i].image.color = Color.red;
 				levels[i].enabled = true;
 			}
 			else {
 				levels[i].image.color = Color.gray;
 				levels[i].enabled = false;	
 			}
-		}*/
+		}
 	}
 
 	public void LoadScene(string level)
