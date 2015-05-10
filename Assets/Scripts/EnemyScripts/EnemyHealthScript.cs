80945f3786f8631928b5e439808e4ed0ef095483
danben4 <danielbenediktsson4@gmail.com>
2015-05-10 17:06:18 +0000

    health stuff

--- Assets/Scripts/EnemyScripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyHealthScript.cs
@@ -6,24 +6,46 @@ public class EnemyHealthScript : MonoBehaviour {
 
 	public int startingHealth;
 	public int currentHealth;
-	public Slider healthSlider;
 	public float flashSpeed = 5f;
 	public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-	public GameObject damImg;
+	//public GameObject damImg;
+	//public Slider healthSlider;
+	Slider healthSlider;
 	Image damageImage;
 	bool isDead;
 	bool damaged;
-	/*
+
 
 	// Use this for initialization
 	void Start () {
-		damageImage = damImg.GetComponent<Image> ();
+		//GameObject go = GameObject.Find ("DamageImage");
+		//GameObject go2 = GameObject.Find ("HealthSlider");
+		var canvas = this.transform.GetComponentInChildren<Transform> ();
+		Transform go;
+		Transform go2;
+		Debug.Log ("letsgo!!");
+		foreach (Transform child in canvas) {
+			Debug.Log ("TEST: " + child.name);
+			go = child.FindChild("DamageImage");
+			go2 = child.FindChild ("HealthSlider");
+			Debug.Log ("HEHE: " + go.name);
+			Debug.Log ("HEHE22: " + go2.name);
+			damageImage = go.GetComponent<Image>();
+
+		}
+
+
+
+		/*
+		damageImage = go.GetComponent<Image> ();
+		healthSlider = go2.GetComponent<Slider> ();
 		currentHealth = startingHealth;
 		healthSlider.maxValue = currentHealth;
 		healthSlider.value = currentHealth;
 		Debug.Log ("TEST: " + damageImage.name);
 		Debug.Log ("TEST: " + damageImage.type);
 		Debug.Log ("TEST: " + healthSlider.name);
+		*/
 	}
 	
 	// Update is called once per frame
@@ -52,6 +74,6 @@ public class EnemyHealthScript : MonoBehaviour {
 	void Death() {
 		isDead = true;
 	}
-	*/
+
 
 }
