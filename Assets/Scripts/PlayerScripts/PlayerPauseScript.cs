7580b5b591508de9ecf6b7b3eadd729ac15fa37d
hilmarragnars <hilmarragnars@gmail.com>
2015-05-10 14:14:56 +0000

    made it possible to pause, simple

---
+++ Assets/Scripts/PlayerScripts/PlayerPauseScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPauseScript : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		//Pause the game when playing.
+		if (Input.GetKey (KeyCode.P)) {
+			if(Time.timeScale == 0){
+				Time.timeScale = 1;
+			}
+			else {
+				Time.timeScale = 0; 
+			}
+		}
+	}
+}
