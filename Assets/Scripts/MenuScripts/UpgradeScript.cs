3c07d3af1e2f2d6800598a0b730a66fa1cdd56d6
hilmarragnars <hilmarragnars@gmail.com>
2015-05-12 14:24:36 +0000

    finished that the player can only upgrate items a certain amount of times

--- Assets/Scripts/MenuScripts/UpgradeScript.cs
+++ Assets/Scripts/MenuScripts/UpgradeScript.cs
@@ -85,16 +85,26 @@ public class UpgradeScript : MonoBehaviour {
 	}
 
 	public void PressMultiCannons(){
-
+		//If the player does not have enough gold
 		if (emptyObject.GetComponent<StoringVarScript> ().goldAmount < secondCannonPrice) {
 			AudioSource.PlayClipAtPoint(errorSound, transform.position);
 			MoneySignal.enabled = true;
-		} else {
-			emptyObject.GetComponent<StoringVarScript> ().secondCannonPrice *= 2;	
-			AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
-			MoneySignal.enabled = false;
-			emptyObject.GetComponent<StoringVarScript> ().SetMultiCannonsTrue ();
-			emptyObject.GetComponent<StoringVarScript> ().goldAmount -= secondCannonPrice;
+		} 
+		//If the player has enough gold.
+		else {
+			//The player can not upgrade again
+			if(emptyObject.GetComponent<StoringVarScript> ().secondCannonCounter >= 1) {
+				AudioSource.PlayClipAtPoint(errorSound, transform.position);
+			}
+			//normal upgrade
+			else {
+				AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
+				MoneySignal.enabled = false;
+				emptyObject.GetComponent<StoringVarScript> ().SetMultiCannonsTrue ();
+				emptyObject.GetComponent<StoringVarScript> ().goldAmount -= secondCannonPrice;
+				emptyObject.GetComponent<StoringVarScript> ().secondCannonCounter++;
+
+			}
 		}
 	}
 
@@ -113,6 +123,7 @@ public class UpgradeScript : MonoBehaviour {
 			if (emptyObject.GetComponent<StoringVarScript> ().healthCounter >= 2) {
 				AudioSource.PlayClipAtPoint(errorSound, transform.position);
 			}
+			//normal upgrade.
 			else {
 				AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
 				MoneySignal.enabled = false;
@@ -155,32 +166,58 @@ public class UpgradeScript : MonoBehaviour {
 	public void PressIncreaseFireRate(){
 
 		if(emptyObject.GetComponent<StoringVarScript> ().goldAmount < fireRatePrice){
+			if (emptyObject.GetComponent<StoringVarScript> ().fireRateCounter >= 2) {
+				MoneySignal.enabled = false;
+			}
+			else {
+				MoneySignal.enabled = true;
+			}
 			AudioSource.PlayClipAtPoint(errorSound, transform.position);
-			MoneySignal.enabled = true;
 		}
 		else {
-			emptyObject.GetComponent<StoringVarScript> ().fireRatePrice *= 2;
-			AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
-			MoneySignal.enabled = false;
-			emptyObject.GetComponent<StoringVarScript> ().fireRate += 0.2f;
-			emptyObject.GetComponent<StoringVarScript> ().goldAmount -= fireRatePrice;
+			//When the player can not upgrade this item further.
+			if (emptyObject.GetComponent<StoringVarScript> ().fireRateCounter >= 2) {
+				AudioSource.PlayClipAtPoint(errorSound, transform.position);
+			}
+			//normal upgrade
+			else{
+				emptyObject.GetComponent<StoringVarScript> ().fireRatePrice *= 2;
+				AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
+				MoneySignal.enabled = false;
+				emptyObject.GetComponent<StoringVarScript> ().fireRate += 0.2f;
+				emptyObject.GetComponent<StoringVarScript> ().goldAmount -= fireRatePrice;
+				emptyObject.GetComponent<StoringVarScript> ().fireRateCounter++;
+			}
 		}
 	}
 
 	public void PressIncreaseSpeed(){
 		
 		if(emptyObject.GetComponent<StoringVarScript> ().goldAmount < speedPrice){
+			if (emptyObject.GetComponent<StoringVarScript> ().speedCounter >= 2) {
+				MoneySignal.enabled = false;
+			}
+			else {
+				MoneySignal.enabled = true;
+			}
 			AudioSource.PlayClipAtPoint(errorSound, transform.position);
-			MoneySignal.enabled = true;
 		}
 		else {
-			emptyObject.GetComponent<StoringVarScript> ().speedPrice *= 2;
-			AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
-			MoneySignal.enabled = false;
-			emptyObject.GetComponent<StoringVarScript> ().speed += 0.05f;
-			emptyObject.GetComponent<StoringVarScript> ().acceleration += 0.025f;
-			emptyObject.GetComponent<StoringVarScript> ().rotateSpeed += 0.25f;
-			emptyObject.GetComponent<StoringVarScript> ().goldAmount -= speedPrice;
+			//When the player can not upgrade this item further.
+			if (emptyObject.GetComponent<StoringVarScript> ().speedCounter >= 2) {
+				AudioSource.PlayClipAtPoint(errorSound, transform.position);
+			}
+			//Normal upgrade
+			else{
+				emptyObject.GetComponent<StoringVarScript> ().speedPrice *= 2;
+				AudioSource.PlayClipAtPoint(upgradeSound, transform.position);
+				MoneySignal.enabled = false;
+				emptyObject.GetComponent<StoringVarScript> ().speed += 0.05f;
+				emptyObject.GetComponent<StoringVarScript> ().acceleration += 0.025f;
+				emptyObject.GetComponent<StoringVarScript> ().rotateSpeed += 0.25f;
+				emptyObject.GetComponent<StoringVarScript> ().goldAmount -= speedPrice;
+				emptyObject.GetComponent<StoringVarScript> ().speedCounter++;
+			}
 		}
 	}
 
@@ -210,7 +247,7 @@ public class UpgradeScript : MonoBehaviour {
 		speedPrice = emptyObject.GetComponent<StoringVarScript> ().speedPrice;
 
 
-
+		//color of the increase health text
 		if (gold < healthPrice) {
 			//The color if the player has reached maximum.
 			if (emptyObject.GetComponent<StoringVarScript> ().healthCounter >= 2) {
@@ -220,6 +257,7 @@ public class UpgradeScript : MonoBehaviour {
 				healthPriceText.color = Color.red;
 			}
 		}
+		//color of the increase damage text
 		if (gold < damagePrice) {
 			//The color if the player has reached maximum.
 			if (emptyObject.GetComponent<StoringVarScript> ().damageCounter >= 3) {
@@ -229,21 +267,39 @@ public class UpgradeScript : MonoBehaviour {
 				damagePriceText.color = Color.red;
 			}
 		}
+		//color of the fireRate text
 		if (gold < fireRatePrice) {
-			fireRatePriceText.color = Color.red;
+			if (emptyObject.GetComponent<StoringVarScript>().fireRateCounter >= 2){
+				fireRatePriceText.color = Color.black;
+			}
+			else{
+				fireRatePriceText.color = Color.red;
+			}
 		}
+		//color of the second cannon text
 		if (gold < secondCannonPrice) {
-			secondCannonPriceText.color = Color.red;
+			if (emptyObject.GetComponent<StoringVarScript>().secondCannonCounter >= 1){
+				secondCannonPriceText.color = Color.black;
+			}
+			else{
+				secondCannonPriceText.color = Color.red;
+			}
 		}
+		//color of the speed text
 		if (gold < speedPrice) {
-			speedPriceText.color = Color.red;
+			if (emptyObject.GetComponent<StoringVarScript>().speedCounter >= 2){
+				speedPriceText.color = Color.black;
+			}
+			else{
+				speedPriceText.color = Color.red;
+			}
 		}
 
 		//The text that the player sees in the upgrade store
 		//increase damage
 		if (emptyObject.GetComponent<StoringVarScript> ().damageCounter >= 3) {
-			damagePriceText.color = Color.black;
 			damagePriceText.text = "Increase Damage of Cannon: \n" + "Reached maximum!";
+			damagePriceText.color = Color.black;
 		} 
 		else {
 			damagePriceText.text = "Increase Damage of Cannon: \n" + emptyObject.GetComponent<StoringVarScript> ().damagePrice + " Coins";
@@ -251,16 +307,39 @@ public class UpgradeScript : MonoBehaviour {
 
 		//increase health
 		if (emptyObject.GetComponent<StoringVarScript> ().healthCounter >= 2) {
-			healthPriceText.color = Color.black;
 			healthPriceText.text = "Increase Health: \n" + "Reached maximum!";
+			healthPriceText.color = Color.black;
 		}
 		else {
 			healthPriceText.text = "Increase Health: \n" + emptyObject.GetComponent<StoringVarScript> ().healthPrice + " Coins";
 		}
 
-		fireRatePriceText.text = "Increase Firerate: \n" + emptyObject.GetComponent<StoringVarScript> ().fireRatePrice + " Coins";
-		secondCannonPriceText.text = "Add cannon to the ship: \n" + emptyObject.GetComponent<StoringVarScript> ().secondCannonPrice + " Coins";
-		speedPriceText.text = "Increase Speed: \n" + emptyObject.GetComponent<StoringVarScript> ().speedPrice + " Coins";
+		//Add cannons to the player's ship
+		if (emptyObject.GetComponent<StoringVarScript> ().secondCannonCounter >= 1) {
+			secondCannonPriceText.text = "Add cannon to the ship: \n" + "Reached maximum!";
+			secondCannonPriceText.color = Color.black;
+		} 
+		else {
+			secondCannonPriceText.text = "Add cannon to the ship: \n" + emptyObject.GetComponent<StoringVarScript> ().secondCannonPrice + " Coins";
+		}
+
+		//increase fireRate
+		if (emptyObject.GetComponent<StoringVarScript> ().fireRateCounter >= 2) {
+			fireRatePriceText.text = "Increase Firerate: \n" + "Reached mamimum!";
+			fireRatePriceText.color = Color.black;
+		} 
+		else {
+			fireRatePriceText.text = "Increase Firerate: \n" + emptyObject.GetComponent<StoringVarScript> ().fireRatePrice + " Coins";
+		}
+
+		//increase speed
+		if (emptyObject.GetComponent<StoringVarScript> ().speedCounter >= 2) {
+			speedPriceText.text = "Increase Speed: \n" + "Reached maximum!";
+			speedPriceText.color = Color.black;
+		} 
+		else {
+			speedPriceText.text = "Increase Speed: \n" + emptyObject.GetComponent<StoringVarScript> ().speedPrice + " Coins";
+		}
 
 	}
 }
