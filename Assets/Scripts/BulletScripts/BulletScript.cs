e5ea10a8d330cd54778dd481e9c5b257ad277ce7
danben4 <danielbenediktsson4@gmail.com>
2015-05-12 09:21:25 +0000

    flot2

--- Assets/Scripts/BulletScripts/BulletScript.cs
+++ Assets/Scripts/BulletScripts/BulletScript.cs
@@ -10,7 +10,10 @@ public class BulletScript : MonoBehaviour {
 
 	}
 
-	void OnTriggerEnter2D() {
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "EnemyBullet") {
+			//Debug.Log ("EXPLOSION!");
+		}
 		Destroy (gameObject);
 	}
 
