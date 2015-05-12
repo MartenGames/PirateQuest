5654eeb4489acb364c6869a3d67c8fd5c9ac2060
sindrikaldal <sindrimars@gmail.com>
2015-05-12 14:34:13 +0000

    working on level 5

---
+++ Assets/Scripts/LevelScripts/FoodLevelScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FoodLevelScript : MonoBehaviour {
+
+
+	public Text timerText;
+	public float timer;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player(Clone)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timerText.text = "" + timer;
+
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+		}
+
+		if (timer <= 0) {
+			timer = 0;
+			if(player != null) {
+				player.GetComponent<PlayerHealthScript>().currentHealth = 0;
+			}
+		}
+
+	}
+}	
