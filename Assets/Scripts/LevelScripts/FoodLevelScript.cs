ff61b505b0b73f3799836f2d3fbe468ec1c6e15c
sindrikaldal <sindrimars@gmail.com>
2015-05-12 14:49:25 +0000

    working on level 5

--- Assets/Scripts/LevelScripts/FoodLevelScript.cs
+++ Assets/Scripts/LevelScripts/FoodLevelScript.cs
@@ -7,10 +7,12 @@ public class FoodLevelScript : MonoBehaviour {
 
 	public Text timerText;
 	public float timer;
+	bool won;
 	GameObject player;
 
 	// Use this for initialization
 	void Start () {
+		won = false;
 		player = GameObject.Find ("Player(Clone)");
 	}
 	
@@ -19,7 +21,11 @@ public class FoodLevelScript : MonoBehaviour {
 
 		timerText.text = "" + timer;
 
-		if (timer > 0) {
+		if (GameObject.FindWithTag ("Food") == null) {
+			won = true;
+		}
+
+		if (timer > 0 && !won) {
 			timer -= Time.deltaTime;
 		}
 
