9f3486584989d7ce66d49c7b2552bb7189a590d4
Jorundur <jorundurj@gmail.com>
2015-05-12 15:03:17 +0000

    huge mute unmute commit

---
+++ Assets/Scripts/AudioScripts/MuteButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MuteButtonScript : MonoBehaviour {
+
+	public Button muteButton;
+	public Sprite mute;
+	public Sprite unmute;
+
+	public void ClickMute() {
+		Debug.Log("here");
+		if (AudioListener.volume == 1.0f) {
+			AudioListener.volume = 0.0f;
+			muteButton.image.overrideSprite = mute;
+		} else {
+			AudioListener.volume = 1.0f;
+			muteButton.image.overrideSprite = unmute;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		muteButton = muteButton.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
