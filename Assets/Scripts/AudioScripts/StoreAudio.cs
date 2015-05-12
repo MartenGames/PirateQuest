ef3a4a6b7d0b8035d5a8ef175e0dd1506e5845bc
Jorundur <jorundurj@gmail.com>
2015-05-12 21:05:08 +0000

    upgrade store mute functionality

--- Assets/Scripts/AudioScripts/StoreAudio.cs
+++ Assets/Scripts/AudioScripts/StoreAudio.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StoreAudio : MonoBehaviour {
 
 	public AudioSource storeAudio;
+	public Button muteButton;
+	public Sprite mute;
+	public Sprite unmute;
 	
 	// Use this for initialization
 	void Start () {
 		storeAudio = storeAudio.GetComponent<AudioSource> ();
+		muteButton = muteButton.GetComponent<Button> ();
 	}
 	
 	// Update is called once per frame
@@ -15,8 +20,10 @@ public class StoreAudio : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.M)) {
 			if (AudioListener.volume == 1.0f) {
 				AudioListener.volume = 0.0f;
+				muteButton.image.overrideSprite = mute;
 			} else {
 				AudioListener.volume = 1.0f;
+				muteButton.image.overrideSprite = unmute;
 			}
 		}
 	}
