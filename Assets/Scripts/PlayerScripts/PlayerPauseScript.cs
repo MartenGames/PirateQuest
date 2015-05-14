9e1529f10d9800af8d6c6419f6101c828443de35
hilmarragnars <hilmarragnars@gmail.com>
2015-05-14 13:59:03 +0000

    made the game start a little later

--- Assets/Scripts/PlayerScripts/PlayerPauseScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerPauseScript.cs
@@ -9,6 +9,8 @@ public class PlayerPauseScript : MonoBehaviour {
 	public Sprite pause;
 	public Sprite play;
 
+	private int startCounter = 0;
+
 	void Start () {
 		pauseCanvas = pauseCanvas.GetComponent<Canvas> ();
 		pauseCanvas = GameObject.FindGameObjectWithTag ("PauseCanvas").GetComponent<Canvas> ();
@@ -18,10 +20,26 @@ public class PlayerPauseScript : MonoBehaviour {
 
 	void Update () {
 
+		if(startCounter == 0){
+			StartCoroutine(PauseBeginning());
+		}
 		//Pause the game when playing.
 		if (Input.GetKeyDown (KeyCode.P)) {
 			Pause ();
 		}
+	}
+
+	//Make the scene start after 2 seconds
+	private IEnumerator PauseBeginning() {
+
+		Time.timeScale = 0.1f;
+		float pauseEndTime = Time.realtimeSinceStartup + 4;
+		while (Time.realtimeSinceStartup < pauseEndTime) {
+			Debug.Log ("TimeScale is: " + Time.timeScale);
+			yield return 0;
+		}
+		Time.timeScale = 1;
+		startCounter++;
 	}
 
 	public void Pause(){
@@ -35,12 +53,13 @@ public class PlayerPauseScript : MonoBehaviour {
 				Time.timeScale = 1;
 				pauseCanvas.gameObject.SetActive (false);
 				pauseButton.image.overrideSprite = pause;
+				Debug.Log ("Canvas should be false!!!, the game is on!");
 			} else {
 				GameObject.Find ("EmptyObject(Clone)").GetComponent<StoringVarScript> ().isPaused = true;
 				Time.timeScale = 0;
 				pauseCanvas.gameObject.SetActive (true);
 				pauseButton.image.overrideSprite = play;
-				Debug.Log (this.name);
+				Debug.Log ("Canvas should be true, the game should be paused!" + Time.timeScale);
 			}
 		}
 	}
@@ -50,7 +69,6 @@ public class PlayerPauseScript : MonoBehaviour {
 		GameObject.Find ("EmptyObject(Clone)").GetComponent<StoringVarScript> ().isPaused = false;
 		Time.timeScale = 1;
 		Application.LoadLevel (Application.loadedLevel);
-		Debug.Log(this.name);
 
 	}
 
